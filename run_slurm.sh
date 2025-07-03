cc747f0f5d2a94a49a8016e0479cb2c18981bc55
Steve Abreu <s.abreu@rug.nl>
2025-07-03 04:42:32 +0200

    slurm run script

---
+++ run_slurm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J grpo-debate
+#SBATCH -t 4:00:00
+#SBATCH -p gpu_h100
+#SBATCH -N 1
+#SBATCH --gpus=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=s.abreu@rug.nl
+
+source .venv/bin/activate
+python main.py --config configs/fullseq.yaml
